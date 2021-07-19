30d35d27c1fc437da726634807d83ed0184d07c7
V0l_B1nary <74317064+v0lb1nary@users.noreply.github.com>
2021-07-19 13:40:02 -0300

    Update MMC.py

--- Testolandy/MMC.py
+++ Testolandy/MMC.py
@@ -4,42 +4,46 @@
 def maior_menor(i,j):
   return i if i > j else j
 
-x = 4
-y = 10
-lol = ()
-
-while x > 1 or y > 1:
-  w = maior_menor(x,y)
-  for i in range(2,w+1):
-    # print("contado:",i," x:",x," y:",y," i:",i)
-    if x % i == 0 and y % i == 0:
-      lol += i,
-      x = int(x/i)
-      y = int(y/i)
-      break
-
-    elif x % i == 0 or y % i == 0:
-      if x % i == 0:
-        lol += i,
-        x = int(x/i)
+def MMD(n1,n2):
+  """ Retorna o Minino Multiplo Comum de 2 numeros """
+  
+  list_MMD = ()
+  while n1 > 1 or n2 > 1:
+    w = maior_menor(n1,n2)
+    for i in range(2,w+1):
+      # print("contado:",i," x:",x," y:",y," i:",i)
+      if n1 % i == 0 and n2 % i == 0:
+        list_MMD += i,
+        n1 = int(n1/i)
+        n2 = int(n2/i)
         break
 
-      if y % i == 0:
-        lol += i,
-        y = int(y/i)
-        break
+      elif n1 % i == 0 or n2 % i == 0:
+        if n1 % i == 0:
+          list_MMD += i,
+          n1 = int(n1/i)
+          break
 
-    else:
+        if n2 % i == 0:
+          list_MMD += i,
+          n2 = int(n2/i)
+          break
+
+      else:
+        pass
+
       pass
-
     pass
-  pass
 
-print(lol)
+  result = 1
+  for i in list_MMD:
+    result *= i
 
-result = 1
-for i in lol:
-  result *= i
+  return(result)
 
-print(result)
-      
+
+x = int(input("N1: "))
+y = int(input("N2: "))
+
+print(MMD(x,y))
+        
